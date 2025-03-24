594cb4674bfc13519494c1eea32776f7afbec920
Renata <murzinarenata@gmail.com>
2025-03-24 14:39:33 -0700

    merge

--- function_d.py
+++ function_d.py
@@ -2,17 +2,14 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-<<<<<<< HEAD
     if len(numbers):
         return numbers[0]
     else:
         return None
     return "Hello day!"
 
-=======
     print("test")
     return "Hello!"
->>>>>>> 09c363a31a1761b74f7fc42f4c25b4ebeda4af71
 
 
 if __name__ == "__main__":
